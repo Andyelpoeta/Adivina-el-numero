3591cf3eef5379eaf8c229918e0a3aa0b2730dc0
andyelpoeta <sotorodriguez4140@gmail.com>
2023-11-01 10:08:05 -0500

    Juego terminado y faltan modificaciones

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Adivina-el-numero
-Un juego sencillo para adivinar un numero del 1 al 100
+Un juego sencillo para adivinar un numero del 1 al 100 donde el jugador tendra varios intentos hasta que 
+acerque o adivine correctamente.
---
+++ script.js
@@ -0,0 +1,54 @@
+
+/** Esto nos permite crear un numero alo zar */
+let numeroAlAzar =  Math.floor(Math.random() * 100 )+ 1 ,
+    numeroEntrante = document.getElementById('numeroEntrada'), 
+    comprobando = document.getElementById('comporobar'),
+    mensaje = document.getElementById('mensaje'),
+    cantidadIntentos = document.getElementById('cantidadIntentos'),
+
+    intentos=0
+    console.log(numeroAlAzar)
+
+comprobando.addEventListener('click', (e) => {
+      e.preventDefault();
+      intentos ++;
+      cantidadIntentos.textContent=intentos;
+      let numeroIngresado = parseInt(numeroEntrante.value)
+      if (numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)) {
+         mensaje.textContent='Introduce un numero valido entre 1 y 100'
+         mensaje.style.color='black'
+         return
+      }
+      if(numeroIngresado === numeroAlAzar){
+            Swal.fire({
+                  icon: 'success',
+                  title: 'Felicitaciones haz adivinado el numero',
+                  showConfirmButton: false,
+                  timer: 4500
+                })
+                setTimeout(() => {
+                  location.reload();
+                }, 3000);   
+                mensaje.textContent='!Haz ganado'
+                mensaje.style.color='green'    
+                
+      } 
+      else if(numeroIngresado < numeroAlAzar){
+      mensaje.textContent='!Muy alto¡ el numero es mayor al que elejistes'
+      mensaje.style.color='red'
+      }
+      else{
+            mensaje.textContent='!Muy bajo¡ el numero es mas pequeño al que elegiste'
+            mensaje.style.color='blue'
+            
+      }
+      if (numeroIngresado === numeroIngresado) {
+            document.getElementById('numeroEntrada').value=''   
+        }
+      
+}) 
+      
+      
+      
+
+
